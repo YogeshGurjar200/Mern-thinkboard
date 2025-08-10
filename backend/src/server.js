2fb5b5fb3f33362cb5f36ef0808c469752141f59
Yogesh Gurjar <yogesh2000gurjar@gmail.com>
2025-08-10 23:34:28 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"    
 import cors from "cors"
 import dotenv from "dotenv"
+import path from "path"
 
 import connectDB from "./config/db.js"
 import notesrouter from "./routes/notesRoutes.js"
@@ -10,10 +11,15 @@ const app = express();
 
 dotenv.config();
 
+const __dirname = path.resolve()
+
 // Middleware
-app.use(cors({
-    origin:"http://localhost:5173",
-}));
+if (process.env.NODE_ENV !== "production"){
+
+    app.use(cors({
+        origin:"http://localhost:5173",
+    }));
+} 
 
 app.use(express.json()) // this middleware will parse JSON bodes : req.body
 
@@ -25,7 +31,13 @@ app.use(rateLimiter); // ratelimiter for indiviual ip 5 req per 10 sec
 // it will listen to the request with match math routhe and fun the notesrouter function
 app.use("/api/notes", notesrouter)
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist","index.html"))
+})
+}
 // this will first connect to database and start the server
 connectDB().then(() => {
 
